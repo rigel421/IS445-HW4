5e603495bf72139cada546087d6c319b36618e8a
rigel421 <42710881+rigel421@users.noreply.github.com>
2018-10-10 18:07:16 -0700

    Add files via upload
    updated with comments and removal of old code comments

--- main.js
+++ main.js
@@ -8,13 +8,16 @@ function loadText(){
 
     xhr.onload = function(){
         if(this.status == 200){
+            //turns JSON file into a JS object
             var myArr = JSON.parse(this.responseText);
-            //console.log(Object.keys(myArr));
-            ///document.getElementById("first").innerHTML = myArr;
+            //create array to store the emails from JSON file
             var emails = [];
+            //for loop goes through array object and pushes the email key value
+            //to the email array
             for(i = 0;i<myArr.length;i++){
                 emails.push(myArr[i].email);
             }
+            //default sort emails in alphabetical order
             emails.sort();
             document.getElementById("first").innerHTML = "User emails from XMLHttpRequest:" + "\n" + emails;
         }
@@ -23,13 +26,16 @@ function loadText(){
 }
 
 fetch('https://jsonplaceholder.typicode.com/users')
+    //turns fetch response into a json response
     .then((resp)=> resp.json())
-    .then(data => {
+    //this then block uses the same for loop as the XML request but is modified to pull the username
+    .then(data => { 
         var users = [];
         for(i = 0;i<data.length;i++){
             users.push(data[i].username);
+            //sorts the array by string length in ascending order
             users.sort(function(a,b){
-                return a.length - b.length; //ASC, For Descending order use: b - a
+                return a.length - b.length;
               });
         }
         document.getElementById("second").innerHTML = "User Usernames from Fetch:" + "\n" + users;
